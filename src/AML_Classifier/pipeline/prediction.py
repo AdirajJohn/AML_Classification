730036473a4519bc36147a7e4bbb78d55deda8ce
adiraj J <adirajjohn2000@gmail.com>
2024-07-04 08:48:09 +0530

    Small modification to assist model deployment

--- src/AML_Classifier/pipeline/prediction.py
+++ src/AML_Classifier/pipeline/prediction.py
@@ -9,7 +9,7 @@ class PredictionPipeline:
     def predict(self):
         #Preprocess the data
         #load the standard scaler
-        with open("artifacts/model_data/scaler_obj.pkl","rb") as file:
+        with open("model/scaler_obj.pkl","rb") as file:
             sc = pickle.load(file)
 
         pred_data_sc=sc.transform(self.pred_data[['from_bank', 'to_bank', 'amount_received', 'amount_paid']])
@@ -18,7 +18,7 @@ class PredictionPipeline:
         #load the label encoders
         pred_data_le=pd.DataFrame()
         for i in ['receiving_currency', 'Payment_Currency', 'Payment Format']:
-            with open(f"artifacts/model_data/le_{i}.pkl","rb") as file:
+            with open(f"model/le_{i}.pkl","rb") as file:
                 le=pickle.load(file)
             pred_data_le[i]=le.transform(np.array(self.pred_data[[i]]).ravel())
 
@@ -26,7 +26,7 @@ class PredictionPipeline:
         pred_data_scaled=pd.concat([pred_data_le,pred_data_sc],axis=1)
 
         #Load the model
-        with open("artifacts/prepare_base_model/base_model.pkl","rb") as file:
+        with open("model/base_model.pkl","rb") as file:
             model = pickle.load(file)
 
         #predict the output
